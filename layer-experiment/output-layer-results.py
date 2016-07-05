54f25eb1cb20d7a44332b6590708a0b6eedb2eab
Nikolas Wolfe <nikwolfe@amazon.com>
2016-07-04 22:30:04 -0400

    layer ids experiment

--- layer-experiment/output-layer-results.py
+++ layer-experiment/output-layer-results.py
@@ -5,12 +5,16 @@ f = [l.strip() for l in open(fname).readlines()]
 pattern = 'Picking the winner...'
 count = 0
 g = []
+gl = []
 ga = []
 for i in range(len(f)):
 	line = f[i]
 	if pattern in line:
 		count += 1
-		g.append(int(f[i+1].split()[2]))
+		val = int(f[i+1].split()[2])
+		g.append(val)
+		if val > 50: gl.append(2)
+		else: gl.append(1)
 		l = f[i+2]
 		accuracy = f[i+5]
 		if l.startswith('Writing error/gain'):
@@ -20,7 +24,8 @@ for i in range(len(f)):
 			accuracy = lt
 		print(accuracy)
 		ga.append(float(accuracy.strip().split()[1]))
-	
+
+print(len(gl))	
 gt = g[0:100]
 g1 = g[100:200]
 g2 = g[200:300]
@@ -28,11 +33,19 @@ g2 = g[200:300]
 gta = ga[0:100]
 g1a = ga[100:200]
 g2a = ga[200:300]
+
+gtl = gl[0:100]
+g1l = gl[100:200]
+g2l = gl[200:300]
+
 o1 = open(fname.strip('.txt') + '.neuron-rankings.txt','w')
 o2 = open(fname.strip('.txt') + '.neuron-accuracy.txt','w')
+o3 = open(fname.strip('.txt') + '.layer-ids.txt','w')
 for i in range(len(gt)):
 	o1.write(','.join((str(gt[i]),str(g1[i]),str(g2[i]))) + '\n')
 	o2.write(','.join((str(gta[i]),str(g1a[i]),str(g2a[i]))) + '\n')
+	o3.write(','.join((str(gtl[i]),str(g1l[i]),str(g2l[i]))) + '\n')
 o1.close()
 o2.close()	
+o3.close()	
 print("count: " + str(count))
